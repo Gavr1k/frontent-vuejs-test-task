ddaeb3a23bafdff5570df875ba4d8929ce6273cd
Andrey Hauryk <andrey.hauryk@gmail.com>
2024-11-27 20:31:50 +0300

    add solution

--- my-vue-project/src/App.vue
+++ my-vue-project/src/App.vue
@@ -1,8 +1,95 @@
 <template>
+  <section class="header-section">
+    <div class="header-section__wrapper">
+      <div class="block">
+        <template v-for="userItem in userSelectedItems" :key="userItem.id">
+          <itemUI class="user-items__item" :item="userItem"></itemUI>
+        </template>
+        <span>Selected: {{userSelectedItems.length}}/6</span>
+      </div>
+      <div class="block">
+          <template v-if="Object.keys(itemSelected).length">
+            <itemUI class="user-items__item" :item="itemSelected"></itemUI>
+          </template>
+          <template v-else>
+            <span>Selected item</span>
+          </template>
+      </div>
+    </div>
+  </section>
+  <section class="items-section">
+    <div class="user-items">
+      <template v-for="userItem in userItems" :key="userItem.id">
+        <itemUI @click="handleSelectUserItem(userItem)" class="user-items__item" :item="userItem"></itemUI>
+      </template>
+    </div>
+    <div class="selected-items">
+      <template v-for="item in items" :key="item.id">
+        <itemUI @click="handleSelectItem(item)" class="user-items__item" :item="item"></itemUI>
+      </template>
+    </div>
+  </section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import itemUI from './components/itemUi.vue';
+import userItemsMock from './mock-data/userItems';
+import itemsMock from './mock-data/items';
+import {ref} from 'vue';
 
+const userItems = ref(userItemsMock);
+const items = ref(itemsMock);
 
+const userSelectedItems = ref([]);
+const itemSelected = ref({});
+
+const handleSelectUserItem = (item) => {
+  if (userSelectedItems.value.length === 6) return;
+  userSelectedItems.value.push(item);
+};
+
+const handleSelectItem = (item) => {
+  itemSelected.value = item;
+}
+
+</script>
 <style scoped>
+
+.body {
+  box-sizing: border-box;
+}
+
+.header-section {
+  margin-bottom: 20px;
+}
+
+.header-section__wrapper {
+  display: flex;
+  justify-content: space-between;
+}
+
+.block {
+  width: 20%;
+  min-height: 30px;
+  border: 1px solid black;
+}
+
+.items-section {
+  display: flex;
+  justify-content: space-between;
+}
+
+.user-items, .selected-items {
+  border: 1px solid black;
+  border-radius: 10px;
+  width: 40%;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.user-items__item {
+  width: 30%;
+  margin: 1rem;
+}
+
 </style>
---
+++ my-vue-project/src/components/itemUI.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="item">
+    <span>{{ item.name }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Item {
+  id: string;
+  number: string;
+}
+
+const props = defineProps<{
+  item: Item;
+}>();
+</script>
+
+<style scoped>
+
+.item {
+  width: 100%;
+  height: 40px;
+  border: 1px solid black;
+  border-radius: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+span {
+  text-align: center;
+}
+
+</style>
